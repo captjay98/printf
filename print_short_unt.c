31300b440375ed929228b7017c109d1ab7f3a034
Adebimpe Islamiyyah Adefila <95176308+MiyyahCodes@users.noreply.github.com>
2022-04-20 17:40:34 +0100

    Hot fix

--- print_short_unt.c
+++ print_short_unt.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * prinhunt - prints a short unsigned integer
- * @arguments: number to print
- * @buf: buffer pointer
- * @ibuf: index for buffer pointer
+ * prinhunt - prints a short unsigned integer. 
+ * @arguments: number to print. 
+ * @buf: buffer pointer. 
+ * @ibuf: index for buffer pointer. 
  * Return: number of chars printed.
  */
 int prinhunt(va_list arguments, char *buf, unsigned int ibuf)
