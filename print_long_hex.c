97023c527f8f6a4376eedbb81e2d77f7a104aaf2
Adebimpe Islamiyyah Adefila <95176308+MiyyahCodes@users.noreply.github.com>
2022-04-20 17:39:45 +0100

    Fix mistake

--- print_long_hex.c
+++ print_long_hex.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * prinlhex - prints a long decimal in hexadecimal
- * @arguments: input string
- * @buf: buffer pointer
- * @ibuf: index for buffer pointer
- * Return: number of chars printed
+ * prinlhex - prints a long decimal in hexadecimal. 
+ * @arguments: input string. 
+ * @buf: buffer pointer. 
+ * @ibuf: index for buffer pointer. 
+ * Return: number of chars printed. 
  */
 int prinlhex(va_list arguments, char *buf, unsigned int ibuf)
 {
