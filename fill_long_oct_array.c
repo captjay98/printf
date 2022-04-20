99c532b544f1d70c21e3885b30ece8c724991660
captjay98 <captjay98@gmail.com>
2022-04-20 17:48:02 +0100

    Style changes

--- fill_long_oct_array.c
+++ fill_long_oct_array.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
+
 /**
- * fill_long_oct_array - calculates a long octal number. 
- *
+ * fill_long_oct_array - calculates a long octal number.
  * @bnr: array where is stored the binary.
  * @oct: array where is stored the octal.
- *
  * Return: binary array.
  */
 char *fill_long_oct_array(char *bnr, char *oct)
--- print_add.c
+++ print_add.c
@@ -5,7 +5,6 @@
  * @arguments: input address.
  * @buf: buffer pointer.
  * @ibuf: index for buffer pointer. 
- *
  * Return: number of chars printed.
  */
 int print_add(va_list arguments, char *buf, unsigned int ibuf)
